7b50a2c320320f15bc409f8074e486b0d9927805
pavlav <pa.lavrentyev@gmail.com>
2024-01-30 17:36:09 +0300

    Align greeting to the center commit

--- app/src/main/java/com/lavrent/happybirthdaycompose/MainActivity.kt
+++ app/src/main/java/com/lavrent/happybirthdaycompose/MainActivity.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.lavrent.happybirthdaycompose.ui.theme.HappyBirthdayComposeTheme
 
@@ -26,6 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    GreetingText(message = "Happy Birthday Andrey!", from = "From Pavel")
                 }
             }
         }
@@ -33,26 +39,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
+fun GreetingText( // составная функция
+    message: String, // сообщение пощдравление
+    from: String, // от кого поздравление
+    modifier: Modifier = Modifier) {
+    Column( // столбец
+        modifier = modifier.padding(8.dp), // отступы вокруг столбца
+        verticalArrangement = Arrangement.Center
+    ) {
         Text(
-            text = message,
-            fontSize = 100.sp,
-            lineHeight = 116.sp
+            text = message, // текст
+            fontSize = 100.sp, // размер шрифта
+            lineHeight = 116.sp, // высота строки
+            textAlign = TextAlign.Center // выравнивание текста по центру
         )
         Text(
-            text = from,
-            fontSize = 36.sp
+            text = from, // текст
+            fontSize = 36.sp, // размер шрифта
+            modifier = Modifier // модификатор в Compose
+                .padding(16.dp) // отступ подписи
+                .align(alignment = Alignment.End) // выравнивание по правому краю текста "от кого"
         )
     }
 }
 
-@Preview(showBackground = true,
-    showSystemUi = true
+@Preview( // аннотация пердназначена только для предварительного просмотра ан панеле Design
+    showBackground = true, // добавление фона
+    showSystemUi = true // показывает интерфейс смартфона
 )
-@Composable
+@Composable // обозначение составной функци в Compose, эта функция предназначена для преобразования данных в пользовательский интерфейс
 fun BirthdayCardPreview() {
-    GreetingText(message = "Happy Birthday Andrey!",
-        from = "From Pavel"
-    )
+    HappyBirthdayComposeTheme {
+        GreetingText(
+            message = "Happy Birthday Andrey!", // сообщение поздравление
+            from = "From Pavel" // от кого поздравление
+        )
+    }
 }
